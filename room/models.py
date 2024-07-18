39d386c985d28357da338318e298e5346f8061ed
sezal saundarya <sezalsinha011@gmail.com>
2024-07-18 18:59:03 +0530

    project started

---
+++ room/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    # host
+    # topic
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # members
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
