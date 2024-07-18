363ee9854e0d1194457082bc581b147a19aa1cbe
sezal saundarya <sezalsinha011@gmail.com>
2024-07-18 20:17:53 +0530

    room model added

--- room/views.py
+++ room/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from room.models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class GetAllRoom(APIView):
+    def get(self,request):
+        room_names = Room.objects.all().values()
+        return Response(data = room_names , status=200)
